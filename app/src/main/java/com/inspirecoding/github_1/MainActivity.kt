7f5f373d41f31728031a34c559224953fbdf7149
inspire_coding <+PaZ+PeTi+88113+>
2020-01-25 12:47:46 +0100

    added click listener

--- app/src/main/java/com/inspirecoding/github_1/MainActivity.kt
+++ app/src/main/java/com/inspirecoding/github_1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.inspirecoding.github_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +11,9 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
